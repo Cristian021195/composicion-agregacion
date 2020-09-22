eaca72f8aedd8f138d1fcd40b486ffb871c6b0b2
unknown <cristian021195>
2020-09-21 21:10:04 -0300

    se agrego ejemplo de composicion / agregacion

---
+++ ComposicionAgregacion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComposicionAgregacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public class Motor{
+            private int potencia;
+            private string marca;
+
+            public int Potencia { get => potencia; set => potencia = value; }
+            public string Marca { get => marca; set => marca = value; }
+        }
+        public class Rueda
+        {
+            private float peso;
+
+            public float Peso { get => peso; set => peso = value; }
+        }
+        public class Vehiculo {
+            private Motor motor;//composicion
+            public List<Rueda> Ruedas { get; set; }//agregacion
+            public Vehiculo(List<Rueda> Ruedas)
+            {
+                this.Ruedas = Ruedas;
+                motor = new Motor();//composicion
+            }
+            public void AgregarRueda(Rueda NuevaRueda)//agregacion
+            {
+                Ruedas.Add(NuevaRueda);//agregacion
+            }
+        }
+    }
+}
